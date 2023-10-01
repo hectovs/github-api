ea8cceb7f23173174bc32b5df13d3f2a99234795
hectovs <htjvans@gmail.com>
2023-10-01 21:29:21 +0100

    required packages and stub

---
+++ src/components/repoDetails.js
@@ -0,0 +1,16 @@
+import useOcto from "../hooks/useOcto"
+
+
+function RepoDetails({user, repo }){
+    const repo = useOcto()
+
+    useEffect(()=>{ 
+
+    }, )
+
+    return(
+        <div></div>
+    )
+}
+
+export default RepoDetails

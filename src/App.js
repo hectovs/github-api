811c5ec43d50a93fa3a122dab109c28024f6f6ea
hectovs <htjvans@gmail.com>
2023-10-01 18:08:17 +0100

    auth success

--- src/App.js
+++ src/App.js
@@ -1,23 +1,29 @@
 import logo from './logo.svg';
 import './App.css';
+import { Octokit } from "@octokit/core"
+import { useEffect } from 'react';
 
 function App() {
+
+  
+  useEffect(()=>{
+    const patToken = process.env.REACT_APP_GITHUB_PAT
+    const octokit = new Octokit({auth:`${patToken}`})
+    
+    const getUser = async ()=>{ 
+      console.log(patToken)
+      const {data} = await octokit.request("/user")
+      console.log(data)
+    }
+
+    getUser()
+  }
+  ,[])
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
     </div>
   );
 }

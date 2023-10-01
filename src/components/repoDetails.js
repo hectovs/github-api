8e2cab464fcc1707d5e986990238d268d143f2cc
hectovs <htjvans@gmail.com>
2023-10-01 21:50:50 +0100

    display details without readme

--- src/components/repoDetails.js
+++ src/components/repoDetails.js
@@ -1,16 +1,27 @@
+import { Card, CardContent } from "@mui/material"
 import useOcto from "../hooks/useOcto"
 import { useEffect } from "react"
 
 
-function RepoDetails({user, repo }){
+function RepoDetails({repo}){
     const repoContent = useOcto()
 
     useEffect(()=>{ 
 
-    }, )
+
+    }, [])
 
     return(
-        <div></div>
+        <div>
+            <Card>
+                <CardContent>
+                    <p>stars: {repo.stargazers_count}</p>
+                    <p>issues: {repo.open_issues} </p>
+                    <p>forks: {repo.forks_count}</p>
+                    
+                </CardContent>
+            </Card>
+        </div>
     )
 }
 
